bcf23e0b5540a52c5d4de566b8900d38b6c8a389
bibito- <w3405034@gmail.com>
2024-09-28 11:45:47 +0900

    Initial Commit

---
+++ SimplePencilkit/SimplePencilkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePencilkitApp.swift
+//  SimplePencilkit
+//
+//  Created by 小林達弥 on 2024/09/28.
+//
+
+import SwiftUI
+
+@main
+struct SimplePencilkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimplePencilkitTests/SimplePencilkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePencilkitTests.swift
+//  SimplePencilkitTests
+//
+//  Created by 小林達弥 on 2024/09/28.
+//
+
+import Testing
+@testable import SimplePencilkit
+
+struct SimplePencilkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
